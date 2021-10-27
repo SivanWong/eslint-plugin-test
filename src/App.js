ae7f515ca43c8850820f0767edcdb6646f91cce3
Xinwei Wangxw <xinwei.wangxw@shopee.com>
2021-10-27 17:33:17 +0800

    eslint-plugin-test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("test");
+  // todo
+
   return (
     <div className="App">
       <header className="App-header">
